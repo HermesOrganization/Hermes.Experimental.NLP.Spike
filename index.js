53a4748985104e7a467a8d05e5d9ff30babfdb90
Chandresh Rajkumar Manonmani <chan@tqube.com>
2016-11-04 06:56:12 +0530

    Webhook Mod

--- index.js
+++ index.js
@@ -27,11 +27,15 @@ app.get('/', (req, res) => {
   res.json({'message': 'Hello World'});
 });
 
-app.get('/webhook/', (req, res) => {
-  if (req.query['hub.verify_token'] === 'YOUR_VERIFY_TOKEN') {
-    res.send(req.query['hub.challenge'])
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === "hermes") {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
   }
-  res.send('Error, wrong token')
 });
 
 app.listen(PORT);
