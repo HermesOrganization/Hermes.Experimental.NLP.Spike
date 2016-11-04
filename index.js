4585323f40f4a67e65c5c2cb849ec462220a4d74
Chandresh Rajkumar Manonmani <chan@tqube.com>
2016-11-04 05:58:43 +0530

    PORT Changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import errorHandler from 'errorhandler';
 
-const PORT = 18000;
+const PORT =  Number(process.env.PORT || 18000);
 
 const app = express();
 
