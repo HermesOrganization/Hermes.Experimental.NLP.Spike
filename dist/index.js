38015f255f20d37a9cfafa57aad12b638191be2e
Chandresh Rajkumar Manonmani <chan@tqube.com>
2016-11-05 00:57:54 +0530

    Message Added

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
@@ -20,11 +22,82 @@ var PORT = Number(process.env.PORT || 18000);
 
 var app = (0, _express2.default)();
 
-var HermesNLP = function HermesNLP() {
-  _classCallCheck(this, HermesNLP);
+var HermesNLP = function () {
+  function HermesNLP() {
+    _classCallCheck(this, HermesNLP);
 
-  return "Hermes NLP";
-};
+    return "Hermes NLP";
+  }
+
+  _createClass(HermesNLP, [{
+    key: 'receivedMessage',
+    value: function receivedMessage(event) {
+      var senderID = event.sender.id;
+      var recipientID = event.recipient.id;
+      var timeOfMessage = event.timestamp;
+      var message = event.message;
+
+      console.log("Received message for user %d and page %d at %d with message: ", senderID, recipientID, timeOfMessage);
+      console.log(JSON.stringify(message));
+
+      var messageId = message.mid;
+      var messageText = message.text;
+      var messageAttachments = message.attachments;
+
+      if (messageText) {
+        switch (messageText) {
+          case 'generic':
+            sendGenericMessage(senderID);
+            break;
+
+          default:
+            sendTextMessage(senderID, messageText);
+        }
+      } else if (messageAttachments) {
+        sendTextMessage(senderID, "Message with attachment received");
+      }
+    }
+  }, {
+    key: 'sendGenericMessage',
+    value: function sendGenericMessage(recipientId, messageText) {}
+  }, {
+    key: 'sendTextMessage',
+    value: function sendTextMessage(recipientId, messageText) {
+      var messageData = {
+        recipient: {
+          id: recipientId
+        },
+        message: {
+          text: messageText
+        }
+      };
+
+      callSendAPI(messageData);
+    }
+  }, {
+    key: 'callSendAPI',
+    value: function callSendAPI(messageData) {
+      request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: 'EAANRZBVDu9vABAMAFpwvtEMaPixOKndOmo2yPREZApvsleMlmgY6cLKD1A7MfegFyELRvYz7ZBjgjpKiKRtsYBdY1YAClFg62CTcI5dWbp6nK5FusyKRx2wrHCZBhS9gH6piPOKDx56CkRBBE8Tl9I8gtGSAByyV4yBDJxqBNLYV6pwJZBhg9' },
+        method: 'POST',
+        json: messageData
+      }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var recipientId = body.recipient_id;
+          var messageId = body.message_id;
+          console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
+        } else {
+          console.error("Unable to send message.");
+          console.error(response);
+          console.error(error);
+        }
+      });
+    }
+  }]);
+
+  return HermesNLP;
+}();
 
 var hermes = new HermesNLP();
 
--- index.js
+++ index.js
@@ -11,6 +11,70 @@ class HermesNLP {
   constructor () {
     return "Hermes NLP";
   }
+
+  receivedMessage (event) {
+    let senderID = event.sender.id;
+    let recipientID = event.recipient.id;
+    let timeOfMessage = event.timestamp;
+    let message = event.message;
+
+    console.log("Received message for user %d and page %d at %d with message: ", senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+
+    let messageId = message.mid;
+    let messageText = message.text;
+    let messageAttachments = message.attachments;
+
+    if (messageText) {
+      switch (messageText) {
+        case 'generic':
+          sendGenericMessage(senderID);
+          break;
+
+        default:
+          sendTextMessage(senderID, messageText);
+      }
+    } else if (messageAttachments) {
+      sendTextMessage(senderID, "Message with attachment received");
+    }
+  }
+
+  sendGenericMessage (recipientId, messageText) {
+
+  }
+
+  sendTextMessage (recipientId, messageText) {
+    let messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        text: messageText
+      }
+    };
+
+    callSendAPI(messageData);
+  }
+
+  callSendAPI (messageData) {
+    request({
+      uri: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: { access_token: 'EAANRZBVDu9vABAMAFpwvtEMaPixOKndOmo2yPREZApvsleMlmgY6cLKD1A7MfegFyELRvYz7ZBjgjpKiKRtsYBdY1YAClFg62CTcI5dWbp6nK5FusyKRx2wrHCZBhS9gH6piPOKDx56CkRBBE8Tl9I8gtGSAByyV4yBDJxqBNLYV6pwJZBhg9' },
+      method: 'POST',
+      json: messageData
+    }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var recipientId = body.recipient_id;
+        var messageId = body.message_id;
+        console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
+      } else {
+        console.error("Unable to send message.");
+        console.error(response);
+        console.error(error);
+      }
+    });
+  }
+
 }
 
 const hermes = new HermesNLP();
